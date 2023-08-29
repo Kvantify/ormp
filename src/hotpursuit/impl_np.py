fd0aac762f5be073a4e295e4ab3300f8c1875e8d
Søren Fuglede Jørgensen <github@fuglede.dk>
2023-08-29 14:53:00 +0200

    Remove dead code left over from previous NumPy implementation

--- src/hotpursuit/impl_np.py
+++ src/hotpursuit/impl_np.py
@@ -1,39 +1,6 @@
 import numpy as np
 
 
-def add_column(X, A_inv, u, dtype):
-    X_prime = np.column_stack((X, u))
-    w = X.T @ u
-    A_inv_w = A_inv @ w
-    # We assume X is normalized so u.T @ u = 1
-    one_by_denominator = 1 / (1 - w.T @ A_inv_w)
-    A_prime_inv_bottom_right = one_by_denominator
-    n = A_inv.shape[0]
-    if n == 0:
-        A_prime_inv = np.array([[A_prime_inv_bottom_right]])
-    else:
-        numerator = np.outer(A_inv_w, A_inv_w.T)
-        A_prime_inv_top_left = A_inv + numerator * one_by_denominator
-        off_diag = -A_inv_w * one_by_denominator
-        # The new A⁻¹ is a block matrix whose top left n x n block
-        # is the original A⁻¹ matrix; it turns out that creating this
-        # block matrix from an empty matrix is faster than using e.g. np.block
-        A_prime_inv = np.empty((n + 1, n + 1), dtype=dtype)
-        A_prime_inv[:n, :n] = A_prime_inv_top_left
-        A_prime_inv[-1, :n] = off_diag
-        A_prime_inv[:n, -1] = off_diag.T
-        A_prime_inv[-1, -1] = A_prime_inv_bottom_right
-    return X_prime, A_prime_inv
-
-
-def eval_curr(X, A_inv, y):
-    proj = X.T @ y
-    coef = A_inv @ proj
-    y_hat = X @ coef
-    residual = y_hat - y
-    return np.dot(residual, residual)
-
-
 def hot_pursuit(X, y, n_nonzero_coefs, tol, greediness):
     dtype = np.float32
     X = X.astype(dtype)
